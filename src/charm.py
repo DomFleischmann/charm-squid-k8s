b18c38eb7f9c9310bcfe72cabc140b4b6e24aee4
David Garcia <david.garcia@canonical.com>
2021-03-12 10:34:37 +0100

    Remove lines of code

--- src/charm.py
+++ src/charm.py
@@ -12,19 +12,6 @@ from oci_image import OCIImageResource, OCIImageResourceError
 
 logger = logging.getLogger(__name__)
 
-EXPORTER_CONTAINER = {
-    "name": "exporter",
-    "image": "prom/node-exporter",
-    "ports": [
-        {
-            "containerPort": 9100,
-            "name": "exporter-http",
-            "protocol": "TCP",
-        }
-    ],
-}
-
-
 class SquidCharm(CharmBase):
     """Class reprisenting this Operator charm."""
 
@@ -34,26 +21,10 @@ class SquidCharm(CharmBase):
         self.image = OCIImageResource(self, "image")
 
         self.framework.observe(self.on.config_changed, self.configure_pod)
-        self.framework.observe(self.on["add_url"].action, self._on_add_url_action)
-        self.framework.observe(self.on["delete_url"].action, self._on_delete_url_action)
-
-        self.framework.observe(
-            self.on["prometheus-target"].relation_joined,
-            self._publish_prometheus_target_info,
-        )
 
         self.cluster = SquidCluster(self, "cluster")
         self.framework.observe(self.on["cluster"].relation_changed, self.configure_pod)
 
-    def _publish_prometheus_target_info(self, event):
-        event.relation.data[self.unit]["host"] = self.app.name
-        event.relation.data[self.unit]["port"] = str(9100)
-
-    def _on_add_url_action(self, event):
-        self.cluster.add_url(event.params["url"])
-
-    def _on_delete_url_action(self, event):
-        self.cluster.delete_url(event.params["url"])
 
     def configure_pod(self, event):
         if not self.unit.is_leader():
@@ -62,11 +33,6 @@ class SquidCharm(CharmBase):
         try:
             self.unit.status = MaintenanceStatus("Fetching image information")
             image_info = self.image.fetch()
-
-            # if not self.cluster.is_ready():
-            #     self.unit.status = BlockedStatus("cluster not ready")
-            #     event.defer()
-            #     return
 
             self.unit.status = MaintenanceStatus("Applying pod spec")
             containers = [
@@ -94,8 +60,6 @@ class SquidCharm(CharmBase):
                     ],
                 }
             ]
-            if self.config.get("enable-exporter"):
-                containers.append(EXPORTER_CONTAINER)
 
             self.model.pod.set_spec({"version": 3, "containers": containers})
 
